59ee447f383913ea3e2c135a2383e91e3309e6c5
0xtusharganotra <ganotra.vox@gmail.com>
2025-08-15 16:20:32 +0530

    arrays in ts

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,63 @@
 "use strict";
-//Assignment
 Object.defineProperty(exports, "__esModule", { value: true });
-function greet(user) {
-    console.log(user.name);
+function getMax(nums) {
+    if (nums.length === 0)
+        throw new Error("Array is empty");
+    let maxi = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > maxi) {
+            maxi = nums[i];
+        }
+    }
+    return maxi;
 }
-let u1 = {
-    name: "Tushar",
-    age: 21,
+let nums = [1, 2, 3, 4, 5, 6];
+console.log(getMax(nums));
+let us1 = {
+    name: "tushar",
+    age: 20,
+    addresses: [
+        { city: "address1", pincode: "1100110" },
+        { city: "address2", pincode: "110101" },
+    ],
 };
-let u2 = {
-    name: "TusharAdmin",
-    permissions: "Access to this repo",
+console.log(us1.addresses);
+let userlist = {
+    User: [
+        {
+            firstname: "ramesh",
+            lastname: "dukehs",
+            age: 10,
+        },
+        {
+            firstname: "dalpesh",
+            lastname: "pukku",
+            age: 20,
+        },
+        {
+            firstname: "tuhpesh",
+            lastname: "daab",
+            age: 20,
+        },
+        {
+            firstname: "naaatu",
+            lastname: "kaap",
+            age: 10,
+        },
+        {
+            firstname: "khasi",
+            lastname: "aagayi",
+            age: 17,
+        },
+    ],
 };
-greet(u1);
+function islegal(users) {
+    const legalusers = users.User.filter((u) => {
+        if (u.age > 18) {
+            return u;
+        }
+    });
+    return legalusers;
+}
+console.log(islegal(userlist));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,87 @@
-//Assignment
+function getMax(nums: number[]): number {
+  if (nums.length === 0) throw new Error("Array is empty");
 
-//Create a function that takes either a user or an admin object as an input, and returns a string saying "Welcome, [name]".
+  let maxi = nums[0];
 
-interface Admin {
-  name: string;
-  permissions: String;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > maxi) {
+      maxi = nums[i];
+    }
+  }
+
+  return maxi;
+}
+
+let nums: number[] = [1, 2, 3, 4, 5, 6];
+
+console.log(getMax(nums));
+
+interface Address {
+  city: string;
+  pincode: string;
 }
 
 interface User {
   name: string;
   age: number;
+  addresses: Address[];
 }
 
-type usertype = User | Admin;
-function greet(user: usertype) {
-  console.log(user.name);
+let us1: User = {
+  name: "tushar",
+  age: 20,
+  addresses: [
+    { city: "address1", pincode: "1100110" },
+    { city: "address2", pincode: "110101" },
+  ],
+};
+
+console.log(us1.addresses);
+
+//Given a list of user try to filter out the ones that are above the age of 18
+
+interface USER {
+  firstname: string;
+  lastname: string;
+  age: number;
 }
 
-let u1: User = {
-  name: "Tushar",
-  age: 21,
-};
+let userlist: USER[] = [
+  {
+    firstname: "ramesh",
+    lastname: "dukehs",
+    age: 10,
+  },
+  {
+    firstname: "dalpesh",
+    lastname: "pukku",
+    age: 20,
+  },
+  {
+    firstname: "tuhpesh",
+    lastname: "daab",
+    age: 20,
+  },
+  {
+    firstname: "naaatu",
+    lastname: "kaap",
+    age: 10,
+  },
+  {
+    firstname: "khasi",
+    lastname: "aagayi",
+    age: 17,
+  },
+];
 
-let u2: Admin = {
-  name: "TusharAdmin",
-  permissions: "Access to this repo",
-};
+function islegal(users: USER[]) {
+  const legalusers = users.filter((u) => {
+    if (u.age > 18) {
+      return u;
+    }
+  });
 
-greet(u1);
+  return legalusers;
+}
+
+console.log(islegal(userlist));
