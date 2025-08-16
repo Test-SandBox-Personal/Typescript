4cbcf91a5b0365a00d0a4370ab5c9c2f14619da7
0xtusharganotra <ganotra.vox@gmail.com>
2025-08-16 17:33:02 +0530

    chore:generic in ts

--- dist/index.js
+++ dist/index.js
@@ -1,63 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function getMax(nums) {
-    if (nums.length === 0)
-        throw new Error("Array is empty");
-    let maxi = nums[0];
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] > maxi) {
-            maxi = nums[i];
-        }
-    }
-    return maxi;
+//return the first element of array now input array can be string or number or any custom type;
+//This is how generic works
+function getel(arg) {
+    return arg[0];
 }
-let nums = [1, 2, 3, 4, 5, 6];
-console.log(getMax(nums));
-let us1 = {
-    name: "tushar",
-    age: 20,
-    addresses: [
-        { city: "address1", pincode: "1100110" },
-        { city: "address2", pincode: "110101" },
-    ],
-};
-console.log(us1.addresses);
-let userlist = {
-    User: [
-        {
-            firstname: "ramesh",
-            lastname: "dukehs",
-            age: 10,
-        },
-        {
-            firstname: "dalpesh",
-            lastname: "pukku",
-            age: 20,
-        },
-        {
-            firstname: "tuhpesh",
-            lastname: "daab",
-            age: 20,
-        },
-        {
-            firstname: "naaatu",
-            lastname: "kaap",
-            age: 10,
-        },
-        {
-            firstname: "khasi",
-            lastname: "aagayi",
-            age: 17,
-        },
-    ],
-};
-function islegal(users) {
-    const legalusers = users.User.filter((u) => {
-        if (u.age > 18) {
-            return u;
-        }
-    });
-    return legalusers;
-}
-console.log(islegal(userlist));
+const el1 = getel(["s1", "s2", "s3"]);
+const el2 = getel([1, 2, 3, 4, 5, 6]);
+const el3 = getel(["s1", 1, 2, 3]);
+console.log(el3);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,14 @@
-function getMax(nums: number[]): number {
-  if (nums.length === 0) throw new Error("Array is empty");
-
-  let maxi = nums[0];
-
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] > maxi) {
-      maxi = nums[i];
-    }
-  }
-
-  return maxi;
+//return the first element of array now input array can be string or number or any custom type;
+//This is how generic works
+function getel<T>(arg: T[]): T {
+  return arg[0];
 }
 
-let nums: number[] = [1, 2, 3, 4, 5, 6];
+const el1 = getel<string>(["s1", "s2", "s3"]);
+const el2 = getel<number>([1, 2, 3, 4, 5, 6]);
 
-console.log(getMax(nums));
+type eltype = number | string;
 
-interface Address {
-  city: string;
-  pincode: string;
-}
+const el3 = getel<eltype>(["s1", 1, 2, 3]);
 
-interface User {
-  name: string;
-  age: number;
-  addresses: Address[];
-}
-
-let us1: User = {
-  name: "tushar",
-  age: 20,
-  addresses: [
-    { city: "address1", pincode: "1100110" },
-    { city: "address2", pincode: "110101" },
-  ],
-};
-
-console.log(us1.addresses);
-
-//Given a list of user try to filter out the ones that are above the age of 18
-
-interface USER {
-  firstname: string;
-  lastname: string;
-  age: number;
-}
-
-let userlist: USER[] = [
-  {
-    firstname: "ramesh",
-    lastname: "dukehs",
-    age: 10,
-  },
-  {
-    firstname: "dalpesh",
-    lastname: "pukku",
-    age: 20,
-  },
-  {
-    firstname: "tuhpesh",
-    lastname: "daab",
-    age: 20,
-  },
-  {
-    firstname: "naaatu",
-    lastname: "kaap",
-    age: 10,
-  },
-  {
-    firstname: "khasi",
-    lastname: "aagayi",
-    age: 17,
-  },
-];
-
-function islegal(users: USER[]) {
-  const legalusers = users.filter((u) => {
-    if (u.age > 18) {
-      return u;
-    }
-  });
-
-  return legalusers;
-}
-
-console.log(islegal(userlist));
+console.log(el3);
